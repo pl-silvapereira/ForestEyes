f5d24af1b3398b64ee58c55b7920da39c2a15685
Pedro Luiz da Silva Pereira <pl.silvapereira@gmail.com>
2026-08-08 02:42:43 -0300

    Update 12-gerarImagensCampanha.py

--- urban-deforestation-monitoring/workspace/scripts/12-gerarImagensCampanha.py
+++ urban-deforestation-monitoring/workspace/scripts/12-gerarImagensCampanha.py
@@ -201,8 +201,8 @@ def main():
             valid = (y_final >= 0) & (y_final < patch_labels.shape[0]) & (x_final >= 0) & (x_final < patch_labels.shape[1])
             patch_labels[y_final[valid], x_final[valid]] = True
 
-            # --- REDIMENSIONAMENTO PARA 520x520 (E NITIDEZ APRIMORADA) ---
-            target_size = 520
+            # --- REDIMENSIONAMENTO PARA 480x480 (E NITIDEZ APRIMORADA) ---
+            target_size = 480
             zoom_y = target_size / patch_rgb.shape[0]
             zoom_x = target_size / patch_rgb.shape[1]
 
